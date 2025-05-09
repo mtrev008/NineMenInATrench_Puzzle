637b3ff3f4ebeccdd4da8663b204447e0d33fa1c
mtrev008 <92137270+mtrev008@users.noreply.github.com>
2025-05-08 20:01:23 -0700

    Fixed bug: Moving multiple spaces if possible
    Allowing moving over multiple spaces if empty instead of only one space at a time

--- CS205_Project_1.py
+++ CS205_Project_1.py
@@ -1,31 +1,68 @@
 import copy
 from collections import deque
+import heapq
+import itertools
+import time
 
+def computeManhattanDistance(state_grid, goal_positions):
+    distance = 0
+    for i in range(len(state_grid)):
+        for j in range(len(state_grid[i])):
+            value = state_grid[i][j]
+            if value > 0:
+                goal_i, goal_j = goal_positions[value]
+                distance += abs(i - goal_i) + abs(j - goal_j)
+    return distance
 
 # Find moves is a function to help find valid soldiers to move
-# It checks if a soldier is at a position and has empty spaces around it, so we can just check those moves 
-# in the future
+# It checks if a soldier is at a position and has empty spaces around it,
+# and generates all possible sliding moves in each direction until an obstacle is hit.
 def find_moves(state):
     # Valid soldiers contains a list of all valid soldiers to move each entry will be a list of x,y
-    valid_soldiers = [] 
+    valid_soldiers = []
 
     for i in range(len(state)):
         for j in range(len(state[i])):
-            if(state[i][j] > 0):
-                if(i > 0 and state[i-1][j]==0):
-                    #print(f'found at {i,j}')
-                    valid_soldiers.append([i,j])
-                elif(i < len(state)-1 and state[i+1][j]==0):
-                    #print(f'found at {i,j}')
-                    valid_soldiers.append([i,j])
-                elif(j > 0 and state[i][j-1]==0):
-                    #print(f'found at {i,j}')
-                    valid_soldiers.append([i,j])                    
-                elif(j < len(state[i])-1 and state[i][j+1]==0):
-                    #print(f'found at {i,j}')
-                    valid_soldiers.append([i,j])     
-                    
+            if state[i][j] > 0:
+                up = i - 1
+                while up >= 0 and state[up][j] == 0:
+                    up -= 1
+                if up + 1 != i:
+                    valid_soldiers.append([i, j, up + 1, j])
+
+                down = i + 1
+                while down < len(state) and state[down][j] == 0:
+                    down += 1
+                if down - 1 != i:
+                    valid_soldiers.append([i, j, down - 1, j])
+
+                left = j - 1
+                while left >= 0 and state[i][left] == 0:
+                    left -= 1
+                if left + 1 != j:
+                    valid_soldiers.append([i, j, i, left + 1])
+
+                right = j + 1
+                while right < len(state[i]) and state[i][right] == 0:
+                    right += 1
+                if right - 1 != j:
+                    valid_soldiers.append([i, j, i, right - 1])
+
     return valid_soldiers
+
+def generate_all_moves(state_value):
+    valid_moves = find_moves(state_value)
+    state_list = []
+    for move in valid_moves:
+        i = move[0]
+        j = move[1]
+        new_i = move[2]
+        new_j = move[3]
+        new_state = copy.deepcopy(state_value)
+        new_state[new_i][new_j] = new_state[i][j]
+        new_state[i][j] = 0
+        state_list.append(new_state)
+    return state_list
 
 def print_solution_path(goal_state):
     path = []
@@ -34,145 +71,59 @@ def print_solution_path(goal_state):
         path.append(current_state.state_value)
         current_state = current_state.parent
 
-    path.reverse() 
+    path.reverse()
 
     for i in range(len(path)):
         for row in path[i]:
             print(row)
-            
-        if (i < len(path)-1):
+
+        if i < len(path) - 1:
             print("TO NEXT")
-        if (i == len(path)-1):
+        if i == len(path) - 1:
             print("GOAL")
 
-
 class State():
-
+    
     def __init__(self, state_value, parent=None, depth=0):
         self.state_value = state_value
         self.depth = depth
         self.parent = parent
 
-    def moveUp(self):
-        valid_moves = find_moves(self.state_value)
-        state_list = []
-        
-        for moves in range(len(valid_moves)):
-            i, j = valid_moves[moves]
-            if(i > 0):
-                if(self.state_value[i-1][j] == 0):
-                    new_state = copy.deepcopy(self.state_value)
-                    new_state[i-1][j] = new_state[i][j]
-                    new_state[i][j] = 0
-                    state_list.append(new_state)
-
-        return state_list
-    
-    def moveDown(self):
-        valid_moves = find_moves(self.state_value)
-        state_list = []
-        
-        for moves in range(len(valid_moves)):
-            i, j = valid_moves[moves]
-            if(i < len(self.state_value)-1):
-                if(self.state_value[i+1][j] == 0):
-                    new_state = copy.deepcopy(self.state_value)
-                    new_state[i+1][j] = new_state[i][j]
-                    new_state[i][j] = 0
-                    state_list.append(new_state)
-        return state_list
-    
-    def moveRight(self):
-        valid_moves = find_moves(self.state_value)
-        state_list = []
-        
-        for moves in range(len(valid_moves)):
-            i, j = valid_moves[moves]
-            if(j < len(self.state_value[i])-1):
-                if(self.state_value[i][j+1] == 0):
-                    new_state = copy.deepcopy(self.state_value)
-                    new_state[i][j+1] = new_state[i][j]
-                    new_state[i][j] = 0
-                    state_list.append(new_state)
-        return state_list
-    
-    def moveLeft(self):
-        valid_moves = find_moves(self.state_value)
-        state_list = []
-        
-        for moves in range(len(valid_moves)):
-            i, j = valid_moves[moves]
-            if(j > 0):
-                if(self.state_value[i][j-1] == 0):
-                    new_state = copy.deepcopy(self.state_value)
-                    new_state[i][j-1] = new_state[i][j]
-                    new_state[i][j] = 0
-                    state_list.append(new_state)
-        return state_list
-
-
 def Uniform_Cost_Search(initial_state, goal_state):
     frontier = deque([initial_state])
-    seen_states = set()  # Using set instead of list like before for way faster lookup
-    
+    seen_states = set()
     while frontier:
-        current_state = frontier.popleft() 
+        current_state = frontier.popleft()
         print("Best state to expand to with g(n) = " + str(current_state.depth) + " is: ")
-        
-        for i in current_state.state_value:
-            print(i)
-            
+        for row in current_state.state_value:
+            print(row)
         if current_state.state_value == goal_state:
             print("FOUND")
             print("Goal reached!")
             print("Path to solution: ")
             print_solution_path(current_state)
             return
-
-        possible_moves = []
-        for state in current_state.moveUp():
-            if state is not None:
-                state_tuple = tuple(tuple(row) for row in state)
-                if state_tuple not in seen_states:
-                    seen_states.add(state_tuple)
-                    temp_state = State(state, current_state, current_state.depth + 1)
-                    frontier.append(temp_state)
-                
-        for state in current_state.moveDown():
-            if state is not None:
-                state_tuple = tuple(tuple(row) for row in state)
-                if state_tuple not in seen_states:
-                    seen_states.add(state_tuple)
-                    temp_state = State(state, current_state, current_state.depth + 1)
-                    frontier.append(temp_state)
-                
-        for state in current_state.moveLeft():
-            if state is not None:
-                state_tuple = tuple(tuple(row) for row in state)
-                if state_tuple not in seen_states:
-                    seen_states.add(state_tuple)
-                    temp_state = State(state, current_state, current_state.depth + 1)
-                    frontier.append(temp_state)
-                
-        for state in current_state.moveRight():
-            if state is not None:
-                state_tuple = tuple(tuple(row) for row in state)
-                if state_tuple not in seen_states:
-                    seen_states.add(state_tuple)
-                    temp_state = State(state, current_state, current_state.depth + 1)
-                    frontier.append(temp_state)
-            
+        possible_moves = generate_all_moves(current_state.state_value)
+        for next_state_value in possible_moves:
+            state_tuple = []
+            for row in next_state_value:
+                state_tuple.append(tuple(row))
+            state_tuple = tuple(state_tuple)
+            if state_tuple not in seen_states:
+                seen_states.add(state_tuple)
+                next_state = State(next_state_value, current_state, current_state.depth + 1)
+                frontier.append(next_state)
     print("Goal not reached.")
 
 
 initial_state = [[-1,-1,-1,0,-1,0,-1,0,-1,-1],
-                [0,2,3,4,5,6,7,8,9,1]]
+                 [0,2,3,4,5,6,7,8,9,1]]
 goal_state = [[-1,-1,-1,0,-1,0,-1,0,-1,-1],
-                [1,2,3,4,5,6,7,8,9,0]]
-
+              [1,2,3,4,5,6,7,8,9,0]]
 
 initial_state = State(initial_state)
 goal_state = State(goal_state)
 
-
+start_time = time.time()
 Uniform_Cost_Search(initial_state, goal_state)
+print("Time elapsed:", round(time.time() - start_time, 2), "seconds")
